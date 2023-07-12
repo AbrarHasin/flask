04bfc63d2e5f369af7f696ca43ddadc484dc85d2
abrarbjit <abrar.hasin@bjitgroup.com>
2023-07-12 17:07:07 +0600

    python manager.py db init worked!!!!!!!!

--- main.py
+++ main.py
@@ -1,48 +1,43 @@
-# from dataclasses import dataclass
-from flask import Flask, current_app
+from dataclasses import dataclass
+from flask import Flask, jsonify, abort
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
-# import requests
+import requests
 
 # from producer import publish
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+CORS(app)
 
 db = SQLAlchemy(app)
 
-CORS(app, supports_credentials=True)
 
-with app.app_context():
-    db.init_app(app)
-    db.session.configure(bind=db.engine)
-
-# @dataclass
+@dataclass
 class Product(db.Model):
-    # id: int
-    # title: str
-    # image: str
+    id: int
+    title: str
+    image: str
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
 
 
-# @dataclass
+@dataclass
 class ProductUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     product_id = db.Column(db.Integer)
 
-    # UniqueConstraint('user_id', 'product_id', name='user_product_unique')
-    __table_args__ = (db.UniqueConstraint('user_id', 'product_id', name='user_product_unique'),)
-
+    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 @app.route('/')
 def index():
-    return 'Hello'
+    return 'Hello enthusiast'
+
 
 # @app.route('/api/products')
 # def index():
